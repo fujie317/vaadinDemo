4da99841c22b45d930206e4e3e3af2ae859fb87e
Fujie Zhang <fzhang556@gmail.com>
2018-01-22 18:35:34 +0800

    first commit

---
+++ src/main/java/com/my/vaadinDemo/CustomerForm.java
@@ -0,0 +1,5 @@
+package com.my.vaadinDemo;
+
+public class CustomerForm {
+
+}
---
+++ src/main/java/com/my/vaadinDemo/CustomerStatus.java
@@ -0,0 +1,5 @@
+package com.my.vaadinDemo;
+
+public enum CustomerStatus {
+	ImportedLead, NotContacted, Contacted, Customer, ClosedLost
+}
